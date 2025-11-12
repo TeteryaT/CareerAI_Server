1ef47e2a8484b3e023aca2dd0cb7cb121efdb270
TeteryaT <mashulec@gmail.com>
2025-11-12 10:19:33 +0300

    add Admin initialization

--- src/models/init.ts
+++ src/models/init.ts
@@ -4,6 +4,7 @@ import { Comment } from "./commentModel";
 import { Speciality } from "./specialityModel";
 import { Answer } from "./answerModel";
 import { setupAssociations } from "./Associations";
+import bcrypt from "bcrypt";
 
 export async function initializeModels(): Promise<void> {
   try {
@@ -14,6 +15,27 @@ export async function initializeModels(): Promise<void> {
 
     await sequelize.sync({ force: false });
     console.log("All models were synchronized successfully.");
+
+    const existingAdmin = await User.findOne({
+      where: { email: "careerai@yandex.ru" },
+    });
+
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash("admin", 10);
+
+      await User.create({
+        name: "Application",
+        surname: "Admin",
+        phone: "nophone",
+        email: "careerai@yandex.ru",
+        password: hashedPassword,
+        role: "admin",
+      });
+
+      console.log("Admin user created successfully.");
+    } else {
+      console.log("Admin user already exists.");
+    }
   } catch (error) {
     console.error("Failed to initialize models:", error);
     process.exit(1);
